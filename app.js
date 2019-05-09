a68a5c43644f66fbbc2ffecaee855de78f3f599b
iamakshatjain <32021987+iamakshatjain@users.noreply.github.com>
2019-05-09 12:22:57 +0530

    setup the basic auth

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express"),
+ 	app = express(),
+	passport = require("passport"),
+	localStrategy = require("passport-local"),
+	mongoose = require("mongoose"),
+	expressSession = require("express-session"),
+	bodyParser = require("body-parser"),
+	dotenv = require("dotenv");
+
+const User = require("./models/user");
+
+const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+app.use(express.static("./public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect(process.env.LDATABASEURL,{useNewUrlParser:true});
+
+app.use((req,res,next) => {
+	res.locals.currentUser = req.user;
+	next(); 
+});
+
+app.use(expressSession({
+	secret:"digidocs",
+	resave:false,
+	saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser);
+passport.deserializeUser(User.deserializeUser);
+
+app.get("/", (req,res) => {
+	res.send("this is going great sir");
+});
+
+app.use("/user",userRoutes);
+app.use("/auth",authRoutes);
+
+app.listen(process.env.PORT || 3000,process.env.IP,() => {
+	console.log("working fine sir ... ");
+});
+
---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new mongoose.Schema({
+	userName:String,
+	password:String,
+	email:String,
+	documents:[String]//this could be converted to an array of objects for documents
+});
+
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User",userSchema);
